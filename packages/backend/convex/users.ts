acb8a16591193e5d25d52c1cce6b4a488259dbae
anshulsharma1710 <workwithfloody@gmail.com>
2025-08-12 18:29:48 +0530

    Authentication-Organization Setup Completed

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -12,6 +12,18 @@ export const getMany = query({
 export const add = mutation({
     args: {},
     handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (identity === null) {
+            throw new Error("Not authenticated");
+        }
+
+        const orgId = identity.orgId as string;
+
+        if (!orgId) {
+            throw new Error("Missing organization");
+        }
+
         const userId = await ctx.db.insert("users", {
             name: "Anshul",
         });
